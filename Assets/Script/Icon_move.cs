9823b024a296bb095d592d6f7b54a43af9f5ff61
ruen_mainpc <ruen346@gmail.com>
2019-07-02 00:10:11 +0900

    프로젝트 분활 및 아이콘 이동 제작

--- Assets/Script/Icon_move.cs
+++ Assets/Script/Icon_move.cs
@@ -4,8 +4,6 @@ using UnityEngine;
 
 public class Icon_move : MonoBehaviour
 {
-    public bool auto_move = true;
-    public bool return_move = true;
     public float delay;
     public float move_x;
     public float move_y;
@@ -14,34 +12,10 @@ public class Icon_move : MonoBehaviour
 
     IEnumerator Start()
     {
-        if(return_move == false)
-        {
-            transform.Translate(-move_x, -move_y, 0);
-        }
+        transform.Translate(-move_x, -move_y, 0);
 
-        if (auto_move == true)
-        {
-            transform.Translate(-move_x, -move_y, 0);
+        yield return new WaitForSeconds(delay);
 
-            yield return new WaitForSeconds(delay);
-
-            while (num != 0)
-            {
-                transform.Translate(move_x / 20, move_y / 20, 0);
-                num--;
-
-                yield return new WaitForSeconds(0.005f);
-            }
-        }
-    }
-
-    public void move()
-    {
-        moves();
-    }
-
-    public IEnumerator moves()
-    {
         while (num != 0)
         {
             transform.Translate(move_x / 20, move_y / 20, 0);
